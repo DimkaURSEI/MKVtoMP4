fce0c4a98d2a6d23862a7740da946796c65f0640
DimkaURSEI <39836799+DimkaURSEI@users.noreply.github.com>
2025-02-05 10:17:40 +0500

    Update MKVtoMP4.py

--- MKVtoMP4.py
+++ MKVtoMP4.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
-import subprocess
+import ffmpeg
 import os
 
 # Функция для выбора исходного файла
@@ -16,21 +16,33 @@ def choose_output_file():
     output_entry.delete(0, tk.END)
     output_entry.insert(0, output_file)
 
-# Функция для выполнения команды ffmpeg
-def run_ffmpeg_command():
+# Функция для выполнения конвертации с использованием ffmpeg-python
+def run_ffmpeg_conversion():
     input_file = input_entry.get()
     output_file = output_entry.get()
-    if input_file and output_file:
-        command = f'ffmpeg -i "{input_file}" -c copy -strict unofficial "{output_file}"'
-        try:
-            subprocess.check_call(command, shell=True)
-            # Команда выполнена успешно, запрашиваем подтверждение удаления исходного файла
-            if messagebox.askyesno("Удаление файла", "Удалить исходный файл после конвертации?"):
-                os.remove(input_file)
-                messagebox.showinfo("Успешно", "Конвертация завершена успешно и исходный файл удален.")
-        except subprocess.CalledProcessError as e:
-            # Обработка ошибки, если команда завершилась с ошибкой
-            messagebox.showerror("Ошибка", f"Ошибка при конвертации: {str(e)}")
+
+    if not input_file or not output_file:
+        messagebox.showerror("Ошибка", "Пожалуйста, выберите исходный и выходной файлы.")
+        return
+
+    try:
+        # Используем ffmpeg-python для выполнения конвертации
+        (
+            ffmpeg
+            .input(input_file)
+            .output(output_file, c='copy', strict='-2')
+            .run(overwrite_output=True, capture_stdout=True, capture_stderr=True)
+        )
+        # Если конвертация успешна, запрашиваем подтверждение удаления исходного файла
+        if messagebox.askyesno("Удаление файла", "Удалить исходный файл после конвертации?"):
+            os.remove(input_file)
+            messagebox.showinfo("Успешно", "Конвертация завершена успешно и исходный файл удален.")
+        else:
+            messagebox.showinfo("Успешно", "Конвертация завершена успешно.")
+    except ffmpeg.Error as e:
+        # Обработка ошибки, если что-то пошло не так
+        error_message = e.stderr.decode('utf-8') if e.stderr else str(e)
+        messagebox.showerror("Ошибка", f"Ошибка при конвертации: {error_message}")
 
 # Создаем главное окно приложения
 app = tk.Tk()
@@ -51,7 +63,7 @@ output_entry.pack()
 output_button = tk.Button(app, text="Выбрать файл", command=choose_output_file)
 output_button.pack()
 
-convert_button = tk.Button(app, text="Конвертировать", command=run_ffmpeg_command)
+convert_button = tk.Button(app, text="Конвертировать", command=run_ffmpeg_conversion)
 convert_button.pack()
 
 # Запускаем главный цикл приложения
